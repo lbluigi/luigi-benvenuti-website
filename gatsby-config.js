02d5619ef4072d6902e256d8f0e41031b7fc94fc
luigi <luigi@luigibenvenuti.com>
2019-08-19 19:23:45 +0200

    FIX: contentful variables name

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,15 @@ if (process.env.ENVIRONMENT !== 'production') {
   dotenv.config();
 }
 
-const { spaceId, accessToken } = process.env;
+const { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } = process.env;
 
 module.exports = {
   plugins: [
     {
       resolve: 'gatsby-source-contentful',
       options: {
-        spaceId,
-        accessToken,
+        spaceId: CONTENTFUL_SPACE_ID,
+        accessToken: CONTENTFUL_ACCESS_TOKEN,
       },
     },
   ],
